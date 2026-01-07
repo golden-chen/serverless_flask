689505b84f6a656a4305c1821f0a134210e2bd59
golden-chen <33325840+golden-chen@users.noreply.github.com>
2026-01-08 04:49:38 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,3 @@
-#from http.server import BaseHTTPRequestHandler
-
-#class handler(BaseHTTPRequestHandler):
- #   def do_GET(self):
-   #     self.send_response(200)
-   #     self.send_header('Content-type', 'text/plain')
-   #     self.end_headers()
-    #    self.wfile.write('Hello from Vercel Serverless Python in index page!'.encode('utf-8'))
-    #    return##
 from flask import Flask, request
 
 app = Flask(__name__)
