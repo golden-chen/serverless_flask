afec5c84b2389457f137b158dffd76784b4a2a57
golden-chen <33325840+golden-chen@users.noreply.github.com>
2026-01-09 01:10:14 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ def say_hello():
     return f"嗨 {name}，這是在 Vercel 運行的 Flask 回傳的！"
 @app.route('/api/test/123')
 @app.route('/test/123')
-def say_hello():
+def say_hello1():
     name = request.args.get('name', 'golden')
     return f"嗨 {name}，這是在 Vercel 123 test 回傳的！"
 if __name__ == "__main__":
