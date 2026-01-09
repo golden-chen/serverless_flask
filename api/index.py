2ba5bda38104f6d8e8d017fac94d9c4a0c3a32cf
golden-chen <33325840+golden-chen@users.noreply.github.com>
2026-01-10 03:34:29 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -19,5 +19,12 @@ def say_hello():
 def say_hello1():
     name = request.args.get('name', 'golden')
     return f"嗨 {name}，這是在 Vercel 123 test 回傳的！"
+@app.route('/add')
+def add_numbers():
+    # 接收前端傳來的 a 和 b
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    result = a + b
+    return f"計算結果：{a} + {b} = {result}"    
 if __name__ == "__main__":
     app.run()
